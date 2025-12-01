6636b25db1c98524ddd16f017d8fe85fe52c9172
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-01 16:06:01 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -271,7 +271,7 @@ import {
             </nav>
 
             {/* Main Content */}
-            <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-40">
+            <main className={`relative z-10 max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 pb-40 ${location.pathname.toLowerCase().startsWith('/game') ? 'px-0' : 'px-4'}`}>
                 {children}
             </main>
         </div>
