8d09a586f5e9fa74845f366ef8221d6bdd106500
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-01 04:42:51 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,21 +1,17 @@
-import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Leaderboard from './pages/Leaderboard';
-import Game from './pages/Game';
 import GameHistory from './pages/GameHistory';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Home": Home,
     "Profile": Profile,
     "Leaderboard": Leaderboard,
-    "Game": Game,
     "GameHistory": GameHistory,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "Profile",
     Pages: PAGES,
     Layout: __Layout,
 };
