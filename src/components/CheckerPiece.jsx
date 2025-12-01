532e82cb9c0d00c8dc787ce964273a00379e79d4
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-01 15:01:54 +0000

    File changes

--- src/components/CheckerPiece.jsx
+++ src/components/CheckerPiece.jsx
@@ -28,14 +28,14 @@ export default function CheckerPiece({ type, isSelected, animateFrom, design = '
         <motion.div
             drag={canDrag}
             dragMomentum={false}
-            dragElastic={0.1}
+            dragElastic={0}
             onDragStart={onDragStart}
             onDragEnd={props.onDragEnd}
             whileDrag={{ scale: 1.2, zIndex: 100, cursor: 'grabbing' }}
             dragSnapToOrigin
             initial={initial}
             animate={animate}
-            transition={{ type: "spring", stiffness: 300, damping: 25 }}
+            transition={{ type: "spring", stiffness: 800, damping: 60 }}
             className={`relative w-[85%] h-[85%] m-auto rounded-full z-10 ${canDrag ? 'cursor-grab active:cursor-grabbing' : 'cursor-default'}`}
             style={{ touchAction: 'none' }}
         >
