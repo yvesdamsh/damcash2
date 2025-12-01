cc39690f30a3528889d46de1fc3c402c7f2cb6e5
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-01 06:49:44 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -209,7 +209,7 @@ import {
             </nav>
 
             {/* Main Content */}
-            <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-40">
                 {children}
             </main>
         </div>
