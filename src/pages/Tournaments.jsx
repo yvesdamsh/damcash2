11d041998ef7b3a71ba4630ead95af7f12ff994c
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-05 00:12:07 +0000

    File changes

--- src/pages/Tournaments.jsx
+++ src/pages/Tournaments.jsx
@@ -510,6 +510,7 @@ export default function Tournaments() {
             )}
 
             {viewMode === 'list' ? (
+            <div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {tournaments
                     .filter(t => {
