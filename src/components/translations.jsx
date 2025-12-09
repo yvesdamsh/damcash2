9c45178a89d1b03e1873983a061d219fb92e0a53
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-09 14:24:33 +0000

    File changes

--- src/components/translations.jsx
+++ src/components/translations.jsx
@@ -1,10 +1,10 @@
 
-import en from '../locales/en.json';
-import fr from '../locales/fr.json';
-import ru from '../locales/ru.json';
-import de from '../locales/de.json';
-import zh from '../locales/zh.json';
-import nl from '../locales/nl.json';
+import en from './locales/en.json';
+import fr from './locales/fr.json';
+import ru from './locales/ru.json';
+import de from './locales/de.json';
+import zh from './locales/zh.json';
+import nl from './locales/nl.json';
 
 export const translations = {
   en,
