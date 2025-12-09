da84bc989b52a34379099de0faa9e29a3c9884b3
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-09 13:00:48 +0000

    File changes

--- src/components/SettingsMenu.jsx
+++ src/components/SettingsMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Settings } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
