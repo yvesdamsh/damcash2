33711771aa7968bcdb1e6216c231420228257afd
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-10 01:33:29 +0000

    File changes

---
+++ src/components/AppTranslations.jsx
@@ -0,0 +1,15 @@
+import en from './locales/en';
+import fr from './locales/fr';
+import ru from './locales/ru';
+import de from './locales/de';
+import zh from './locales/zh';
+import nl from './locales/nl';
+
+export const translations = {
+  en,
+  fr,
+  ru,
+  de,
+  zh,
+  nl
+};
