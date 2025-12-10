ec5a869e102aab63a443d16aa0bfe37df4dd0eb9
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-10 01:19:57 +0000

    File changes

--- src/components/LanguageContext.jsx
+++ src/components/LanguageContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { translations } from './AppTranslations.js';
+import { translations } from './AppTranslations';
 import { format, formatDistanceToNow } from 'date-fns';
 import { enUS, ru, de, zhCN, nl } from 'date-fns/locale';
 
