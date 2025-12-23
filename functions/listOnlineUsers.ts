fce70d0f53289028b785526182e5df37bce586a3
Yves Ahipo OFFICIEL <yves.ahipo@gmail.com>
2025-12-23 14:27:45 +0000

    File changes

--- functions/listOnlineUsers.ts
+++ functions/listOnlineUsers.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 Deno.serve(async (req) => {
   try {
--- functions/walletManager.ts
+++ functions/walletManager.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { z } from 'npm:zod@^3.24.2';
 
 const RATE_LIMITS = new Map();
